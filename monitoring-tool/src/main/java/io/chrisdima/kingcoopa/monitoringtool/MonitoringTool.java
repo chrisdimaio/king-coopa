1cd2bbeded8424d86169e7f4883cb1dc1708bcd8
Chris DiMaio <chrisdimaio@gmail.com>
2021-04-18 09:53:13 -0400

    grabing info from king coopa

--- monitoring-tool/src/main/java/io/chrisdima/kingcoopa/monitoringtool/MonitoringTool.java
+++ monitoring-tool/src/main/java/io/chrisdima/kingcoopa/monitoringtool/MonitoringTool.java
@@ -1,4 +1,62 @@
 package io.chrisdima.kingcoopa.monitoringtool;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import java.io.ByteArrayInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Map;
+
 public class MonitoringTool {
+  public MonitoringTool() {}
+
+  public static String getInfo() throws IOException {
+//    URLConnection connection = new URL("http://192.168.1.9:8080/info").openConnection();
+//    InputStream response = new URL("http://192.168.1.9:8080/info").openStream();
+    StringBuilder builder = new StringBuilder();
+    try (BufferedReader reader = new BufferedReader(
+        new InputStreamReader(new URL("http://192.168.1.9:8080/info").openStream()))) {
+      for (String line; (line = reader.readLine()) != null;) {
+        builder.append(line);
+      }
+    }
+
+    return builder.toString();
+  }
+
+  public static void main(String[] args) {
+    try {
+//      getInfo();
+        /* Amazon S3: */
+         MinioClient minioClient =
+             MinioClient.builder()
+                 .endpoint("https://s3.amazonaws.com")
+                 .credentials(
+                     System.getenv("AWS_ACCESS_KEY"), System.getenv("AWS_SECRET_KEY"))
+                 .build();
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(
+            getInfo().getBytes("UTF-8"));
+
+        minioClient.putObject(
+            PutObjectArgs
+                .builder()
+                .bucket("king-coopa")
+                .object("info.json")
+                .contentType("application/json")
+                .userMetadata(Map.of("x-amz-acl", "public-read"))
+                .stream(bais, bais.available(), -1).build());
+        bais.close();
+        System.out.println("my-objectname is uploaded successfully");
+
+      } catch (Exception e) {
+        System.out.println("Error occurred: " + e);
+      }
+    }
 }
